8c35a313bc25f1bf29f5e41a10df1e9feb133dff
Louise Traberg <louisetrab@gmail.com>
2022-12-20 09:13:42 +0100

    summary

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,18 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        about: resolve(__dirname, "src/about.html"),
+        sitemap: resolve(__dirname, "src/sitemap.html"),
+        categoryempty: resolve(__dirname, "src/category-empty.html"),
+        categorymugs: resolve(__dirname, "src/category-mugs.html"),
+        categoryflags: resolve(__dirname, "src/category-flags.html"),
+        categorywreaths: resolve(__dirname, "src/category-wreaths.html"),
+        singlemugs: resolve(__dirname, "src/singleview-mugs.html"),
+        singlewreaths: resolve(__dirname, "src/singleview-wreaths.html"),
+        singleflags: resolve(__dirname, "src/singleview-flags.html"),
+        purchase: resolve(__dirname, "src/purchase.html"),
+        payflow: resolve(__dirname, "src/payflow.html"),
+
         // about: resolve(__dirname, "src/about.html"),
       },
     },
